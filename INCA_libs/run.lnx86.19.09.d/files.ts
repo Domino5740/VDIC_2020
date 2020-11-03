b4e6c53f14a8d84a4941ed515ed9cbd2a1af158e
Domino5740 <domino5740@o2.pl>
2020-11-03 21:48:15 +0100

    lab02_final

--- INCA_libs/run.lnx86.19.09.d/files.ts
+++
@@ -1,7 +0,0 @@
-1604422317 /home/student/dorlowski/VDIC/mtm_Alu.vp
-1604432335 /home/student/dorlowski/VDIC/alu_pkg.sv
-1604435423 /home/student/dorlowski/VDIC/alu_bfm.sv
-1604433859 /home/student/dorlowski/VDIC/tester.svh
-1604434508 /home/student/dorlowski/VDIC/coverage.svh
-1604433269 /home/student/dorlowski/VDIC/scoreboard.svh
-1604431649 /home/student/dorlowski/VDIC/alu_tb.sv
