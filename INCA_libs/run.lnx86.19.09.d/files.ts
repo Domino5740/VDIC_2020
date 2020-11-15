1ff22286bb94ff42a5e6427b6aaafa2112633836
Domino5740 <domino5740@o2.pl>
2020-11-15 22:34:10 +0100

    gitignore

--- INCA_libs/run.lnx86.19.09.d/files.ts
+++
@@ -1,4 +0,0 @@
-1605469052 /home/student/dorlowski/VDIC/mtm_Alu.vp
-1605472174 /home/student/dorlowski/VDIC/alu_pkg.sv
-1605472292 /home/student/dorlowski/VDIC/alu_bfm.sv
-1605475140 /home/student/dorlowski/VDIC/alu_tb.sv
