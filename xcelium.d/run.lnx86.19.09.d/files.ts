5c5d767db63a1af24cae581bdcea86fb59b758ac
Domino5740 <domino5740@o2.pl>
2020-12-08 19:28:52 +0100

    remove xcelium.d

--- xcelium.d/run.lnx86.19.09.d/files.ts
+++
@@ -1,4 +0,0 @@
-1605625809 /home/student/dorlowski/VDIC/lab05part1/mtm_Alu.vp
-1607167093 /home/student/dorlowski/VDIC/lab05part1/alu_pkg.sv
-1607173564 /home/student/dorlowski/VDIC/lab05part1/alu_bfm.sv
-1605635794 /home/student/dorlowski/VDIC/lab05part1/alu_tb.sv
